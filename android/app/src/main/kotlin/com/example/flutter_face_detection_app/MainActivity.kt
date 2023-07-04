c9a51f466d8b9eb49f6d5fc182d22be8ca15615e
Nguyễn Ngọc Minh Nhật <71200617+nhatnguyen510@users.noreply.github.com>
2023-07-04 15:33:30 +0700

    init

---
+++ android/app/src/main/kotlin/com/example/flutter_face_detection_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_face_detection_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_face_detection_app/screens/login_screen.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  static const String _title = 'Mobifone';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: const Text(_title)),
+        body: const LoginScreen(),
+      ),
+    );
+  }
+}
